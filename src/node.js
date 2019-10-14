de5d459094aee8cc57f5258a7635575f111a3d1c
Yuriy17 <yuriy_sh@zoho.com>
2019-10-14 17:30:44 +0300

    priority-queue/feat: realize appendChild

--- src/node.js
+++ src/node.js
@@ -1,22 +1,52 @@
 class Node {
 	constructor(data, priority) {
-
+		this.data = data;
+		this.priority = priority;
+		this.parent = null;
+		this.left = null;
+		this.right = null;
 	}
 
 	appendChild(node) {
-
+		if (this.left === null) {
+			this.left = node;
+			node.parent = this;
+		} else if (this.right === null) {
+			this.right = node;
+			node.parent = this;
+		}
 	}
 
 	removeChild(node) {
-
+		if (node === this.left) {
+			this.left = null;
+			node.parent = null;
+		} else if (node === this.right) {
+			this.right = null;
+			node.parent = null;
+		} else {
+			throw new Error;
+		}
 	}
 
 	remove() {
-
+		if (this.parent) {
+			this.parent.removeChild(this);
+		}
 	}
 
 	swapWithParent() {
-		
+		if (this.parent) {
+			
+			this.parent.parent=this;
+			this.appendChild(this.parent);
+			if (this === this.parent.left){
+				this.parent.left=null;
+			}else{
+				this.parent.right=null;
+			}
+		}
+
 	}
 }
 
